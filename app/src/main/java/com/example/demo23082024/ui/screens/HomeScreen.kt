1ec36b30bf7f5f7a346fcb3f3406728a674a369b
salonso1602 <santiago.ralonso@correo.ucu.edu.uy>
2024-09-16 20:53:17 -0300

    add action buttons and placeholders for home

--- app/src/main/java/com/example/demo23082024/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/demo23082024/ui/screens/HomeScreen.kt
@@ -2,6 +2,7 @@ package com.example.demo23082024.ui.screens
 
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -10,16 +11,23 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBackIosNew
 import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material.icons.filled.Logout
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.outlined.BookmarkBorder
+import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedIconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -47,124 +55,190 @@ fun HomeScreen(
     userNameState: MutableState<String>,
     onLogoutClick: () -> Unit
 ){
-    Row (
-        horizontalArrangement = Arrangement.SpaceBetween,
-        modifier = Modifier
-            .fillMaxWidth()
-            .background(
-                Brush.verticalGradient(
-                    colors = listOf(
-                        Color(red = 243, green = 159, blue = 57),
-                        Color(red = 237, green = 120, blue = 47),
-                    )
-                )
-            ),
-    ){
-        Column {
-            Row (
-                horizontalArrangement = Arrangement.SpaceBetween,
-                modifier = Modifier
-                    .fillMaxWidth()
-            ){
-                IconButton(
-                    onClick = onLogoutClick,
-                    modifier = Modifier
-                ) {
-                    Icon(
-                        imageVector = Icons.Filled.ArrowBackIosNew,
-                        contentDescription = "Logout")
-                }
-                TextButton(
-                    onClick = { /*TODO*/ }
-                ) {
-                    Text(
-                        text = "Hello " + userNameState.value,
-                        fontWeight = FontWeight.Bold,
-                        fontSize = 18.sp
-                        )
-                }
-                IconButton(
-                    onClick = { /*TODO*/ },
-                    modifier = Modifier
+    Column {
 
-                ) {
-                    Icon(
-                        imageVector = Icons.Filled.Menu,
-                        contentDescription = "Logout")
-                }}
-            Row (
-                horizontalArrangement = Arrangement.SpaceEvenly,
-                modifier = Modifier
-                    .fillMaxWidth()
-            ){
-                Column (
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(0.55f)
+                .background(
+                    Brush.verticalGradient(
+                        colors = listOf(
+                            Color(red = 243, green = 159, blue = 57),
+                            Color(red = 237, green = 120, blue = 47),
+                        )
+                    )
+                ),
+        ) {
+            Column {
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
                     modifier = Modifier
-                        .fillMaxWidth(0.5f)
-                        .fillMaxHeight(0.6f)
-                ){
-                    Column (
-                        verticalArrangement = Arrangement.SpaceEvenly,
-                        modifier = Modifier.fillMaxHeight()
-                    ){
-                        Row {
-                            Column {
-                                val date = LocalDateTime.now()
-                                Text(date.dayOfMonth.toString(),
-                                    fontSize = 36.sp
+                        .fillMaxWidth()
+                ) {
+                    IconButton(
+                        onClick = onLogoutClick,
+                    ) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBackIosNew,
+                            contentDescription = "Logout"
+                        )
+                    }
+                    TextButton(
+                        onClick = { /*TODO*/ }
+                    ) {
+                        Text(
+                            text = "Hello " + userNameState.value,
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 18.sp,
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                    }
+                    IconButton(
+                        onClick = { /*TODO*/ },
+
+                        ) {
+                        Icon(
+                            imageVector = Icons.Filled.Menu,
+                            contentDescription = "Logout"
+                        )
+                    }
+                }
+                Row(
+                    horizontalArrangement = Arrangement.SpaceEvenly,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth(0.5f)
+                            .fillMaxHeight()
+                    ) {
+                        Column(
+                            verticalArrangement = Arrangement.SpaceEvenly,
+                            modifier = Modifier.fillMaxHeight()
+                        ) {
+                            Row {
+                                Column {
+                                    val date = LocalDateTime.now()
+                                    Text(
+                                        date.dayOfMonth.toString(),
+                                        fontSize = 36.sp
+                                    )
+                                    Text(
+                                        date.month.toString(),
+                                        fontSize = 20.sp
+                                    )
+                                }
+                            }
+                            Row {
+                                Column(
+                                    verticalArrangement = Arrangement.SpaceEvenly,
+                                    modifier = Modifier.fillMaxHeight(0.33f)
+                                ) {
+                                    Text(
+                                        "Cochabamba",
+                                        fontSize = 24.sp
+                                    )
+                                    Text(
+                                        "Clean City",
+                                        fontSize = 24.sp
+                                    )
+                                    Text(
+                                        "Together",
+                                        fontSize = 24.sp
+                                    )
+                                }
+                            }
+                            Row {
+                                Icon(
+                                    imageVector = Icons.Filled.LocationOn,
+                                    contentDescription = "Location Icon"
                                 )
-                                Text(date.month.toString(),
+                                Text(
+                                    "Cochabamba, BOL",
                                     fontSize = 20.sp
                                 )
                             }
                         }
-                        Row {
-                            Column (
-                                verticalArrangement = Arrangement.SpaceEvenly,
-                                modifier = Modifier.fillMaxHeight(0.33f)
-                            ){
-                                Text("Cochabamba",
-                                    fontSize = 24.sp
-                                )
-                                Text("Clean City",
-                                    fontSize = 24.sp
-                                )
-                                Text("Together",
-                                    fontSize = 24.sp
-                                )
-                            }
-                        }
-                        Row {
-                            Icon(imageVector = Icons.Filled.LocationOn,
-                                contentDescription = "Location Icon")
-                            Text("Cochabamba, BOL",
-                                fontSize = 20.sp
-                            )
-                        }
                     }
-                }
-                Column (
-                    modifier = Modifier
-                        .fillMaxWidth(0.75f)
-                        .align(Alignment.Bottom)
-                ){
-                    Image(
-                        painter = painterResource(R.drawable.kiryu),
-                        contentDescription = "kir",
-                        contentScale = ContentScale.Crop,
+                    Column(
                         modifier = Modifier
-                            .size(500.dp)
-
-                    )
+                            .fillMaxWidth(0.75f)
+                            .align(Alignment.Bottom)
+                    ) {
+                        Image(
+                            painter = painterResource(R.drawable.kiryu),
+                            contentDescription = "kir",
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier
+                                .heightIn(max = 1200.dp)
+                                .fillMaxWidth()
+                        )
+                    }
                 }
             }
         }
-    }
-    Row (
-        modifier = Modifier
-            .fillMaxWidth()
-            .background(Color.Red)
-    ){
-
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Red)
+        ) {
+            /* joinedFriendsComponent */
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Red)
+        ) {
+            /* activity description */
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Red)
+        ) {
+            /* map of activity */
+        }
+        Row(
+            horizontalArrangement = Arrangement.Absolute.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(60.dp)
+                .align(Alignment.CenterHorizontally)
+        ) {
+            /* activity actions */
+            OutlinedIconButton(
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .fillMaxWidth(0.20f)
+                    .fillMaxHeight(),
+                shape = RoundedCornerShape(16.dp),
+                border =  BorderStroke(color = Color.LightGray, width = 2.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Outlined.BookmarkBorder,
+                    contentDescription = "Bookmark activity",
+                    modifier = Modifier.size(36.dp),
+                    tint = Color.LightGray
+                )
+            }
+            Button(
+                onClick = { /*TODO*/ },
+                shape = RoundedCornerShape(16.dp),
+                modifier = Modifier
+                    .fillMaxWidth(0.80f)
+                    .fillMaxHeight(),
+                colors = ButtonColors(Color.Black, Color.White, Color.DarkGray, Color.White)
+            ) {
+                Text(
+                    text = "Join Now",
+                    fontSize = 20.sp
+                    )
+            }
+        }
     }
 }
 
